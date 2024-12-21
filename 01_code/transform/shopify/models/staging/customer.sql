26a6aaad99a8871dc182d9dfcc83c5f974319531
amar_h <amarhussain135@outlook.com>
2024-12-21 17:50:09 +0000

    dbt project created. Staging.customers model drafted

---
+++ 01_code/transform/shopify/models/staging/customer.sql
@@ -0,0 +1,21 @@
+
+{{ 
+    config(
+        materialized='table'
+    ) 
+}}
+
+WITH raw_cust AS (
+
+SELECT
+  id
+  ,CONCAT(first_name, ' ', last_name) AS customer_name
+  ,email
+  ,phone
+FROM impression-digital.raw.Customer
+
+)
+
+SELECT *
+FROM raw_cust
+
